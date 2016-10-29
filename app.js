a4169be85e17236bf8983d247550a7af40584b09
Michelia Feng <765412321@qq.com>
2016-10-29 14:49:50 +0800

    close verify first

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 // app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 /*
